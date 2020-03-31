8dd739c697760bf28af6afae35d8fb4aa3686f13
nikitashpota <55178710+nikitashpota@users.noreply.github.com>
2020-04-01 00:57:43 +0300

    test

---
+++ ba_roombook/ba_roombook/App.cs
@@ -0,0 +1,26 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+/*test*/
+
+namespace ba_roombook
+{
+    class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
