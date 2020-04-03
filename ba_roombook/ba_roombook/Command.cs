79ba8a8ca19c6f536bb4d57c272e50de20165c8a
nikitashpota <55178710+nikitashpota@users.noreply.github.com>
2020-04-03 22:02:26 +0300

    03.04

--- ba_roombook/ba_roombook/Command.cs
+++ ba_roombook/ba_roombook/Command.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 #endregion
@@ -27,23 +28,25 @@ namespace ba_roombook
 
             // Access current selection
 
+            #region Выбор помещений
             Selection sel = uidoc.Selection;
             ISelectionFilter selFilter = new SelcetionCategorie();
             IList<Reference> rooms = uidoc.Selection.PickObjects(ObjectType.Element, selFilter, "Select rooms");
+            #endregion
 
             // Crete views
 
-            IList <Element> views = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views).WhereElementIsNotElementType().ToElements();
+            #region Поиск вида для ведомости отделки
+            IList<Element> views = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views).WhereElementIsNotElementType().ToElements();
             FilteredElementCollector collector = new FilteredElementCollector(doc);
             collector.OfClass(typeof(ViewFamilyType));
             ViewFamilyType viewFamilyType = collector.Cast<ViewFamilyType>().First(vft => vft.ViewFamily == ViewFamily.Drafting);
-
-            //List<Element> subProducts = Model.subproducts as List<Element>;
+            
             List<Element> views_list = new List<Element>();
-            //IList<Element> obj = `Your Data Will Be Here`;
             views_list = views.ToList<Element>();
             bool result_view = views_list.Any(x => (x as View)?.Name == "Форма 1 Ведомость отделки помещений");
-
+            #endregion
+            #region Создание вида. Проверка на наличе вида.
             if (result_view)
             {
                 View view_rb = views_list.Where(x => (x as View)?.Name == "Форма 1 Ведомость отделки помещений").First() as View;
@@ -60,27 +63,118 @@ namespace ba_roombook
                     tx.Commit();
                 }
             }
+            #endregion
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
+            // Work with geometry rooms
+            #region Заведение опции ограничивающих поверхностей
+            SpatialElementBoundaryOptions spatialElementBoundaryOptions = new SpatialElementBoundaryOptions();
+            spatialElementBoundaryOptions.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish;
+            SpatialElementGeometryCalculator calculator = new SpatialElementGeometryCalculator(doc, spatialElementBoundaryOptions);
+            #endregion
 
-            // Filtered element collector is iterable
+            #region List for roombook
+            List<List<string>> walls = new List<List<string>>();
+            List<List<string>> floors = new List<List<string>>();
+            List<List<string>> ceiling = new List<List<string>>();
+            List<List<string>> columns = new List<List<string>>();
+            List<List<double>> walls_area = new List<List<double>>();
+            List<List<double>> floors_area = new List<List<double>>();
+            List<List<double>> ceiling_area = new List<List<double>>();
+            List<List<double>> columns_area = new List<List<double>>();
+            #endregion
 
-            foreach (Element e in col)
+            foreach (Reference room_ref in rooms)
             {
-                Debug.Print(e.Name);
+                //Создание словаря для ВОП (по категориям Формы 1)
+                RoomDict<string, double> room_dict_wall = new RoomDict<string, double>();
+                //Создание множества для проверки уникальности элемента
+                HashSet<Element> set_element = new HashSet<Element>();
+                Room room = (doc.GetElement(room_ref.ElementId)) as Room;
+
+                SpatialElementGeometryResults room_results = calculator.CalculateSpatialElementGeometry(room);
+                Solid roomSolid = room_results.GetGeometry();
+
+                string msg = string.Empty;
+                foreach (Face roomSolidFace in roomSolid.Faces)
+                {
+                    foreach (SpatialElementBoundarySubface subface in room_results.GetBoundaryFaceInfo(roomSolidFace))
+                    {
+                        Element element_in_room = doc.GetElement(subface.SpatialBoundaryElement.HostElementId);
+                        int categorie_name = (doc.GetElement(element_in_room.Id)).Category.Id.IntegerValue;
+                        set_element.Add(element_in_room);
+                        // For walls
+                        if (categorie_name.Equals((int)BuiltInCategory.OST_Walls))
+                        {
+                            ICollection<ElementId> material_id = element_in_room.GetMaterialIds(false);
+                            foreach (ElementId id in material_id)
+                            {
+                                var param = doc.GetElement(id).LookupParameter("ADSK_Группирование");
+                                //msg += $"\nParameter: {param}\n";
+                                if (param is null) continue;
+                                string string_group = param.AsString();
+                                //msg += $"Parameter: {string_group}\n";
+                                try
+                                {
+                                    if ((string_group == "Отделка") & !(set_element.Contains(element_in_room)))
+                                    {
+                                        double material_area = element_in_room.GetMaterialArea(id, false);
+                                        string material_name = doc.GetElement(id).get_Parameter(BuiltInParameter.MATERIAL_NAME).AsString();
+                                        double area = UnitUtils.ConvertFromInternalUnits(material_area, DisplayUnitType.DUT_SQUARE_METERS);
+                                        var type = element_in_room.GetType();
+                                        if (type.Equals(typeof(FamilyInstance)))
+                                        {
+                                            room_dict_wall[material_name] += area/2;
+                                        }
+                                        else
+                                        {
+                                            room_dict_wall[material_name] += area;
+                                        }
+                                        //msg += $"Material name: {room_dict_wall.Values.Sum()}\n";
+                                    }
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    msg += $"\n\n\t" + ex.Message                                            + ":\n\n\t" + ex.Source                                            + ":\n\n\t" + ex.TargetSite                                            + "\n\n\t" + ex.StackTrace;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                //Добавить в список значение словарей
+                walls.Add(room_dict_wall.Keys.ToList());
+                walls_area.Add(room_dict_wall.Values.ToList());
+
+                
             }
 
-            // Modify document within a transaction
+            #region Create double
+            double lpy_w, lpy_f, lpy_c, apy_w,apy_f, apy_c, lpy_cn, apy_cn = -0.003;
+            var point_Y = 0;
+            #endregion
 
-            /*using (Transaction tx = new Transaction(doc))
+            string prs = string.Empty;
+            foreach (var room_ref in rooms.Select((Value, Index) => new { Value, Index }))
             {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }*/
+
+                prs += $"Index: {room_ref.Value}\n";
+                TaskDialog.Show("SD", prs + $"\nGood work");
+                Room room = (doc.GetElement(room_ref.Value.ElementId)) as Room;
+                var number_room = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString();
+                var name_room = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString();
+                //Создание списка с ID для групппирования
+                List<ElementId> element_id_for_group = new List<ElementId>();
+
+                foreach (var walls_room in walls.Select((Value_wr, Index_wr) => new { Value_wr, Index_wr }))
+                {
+                    var text_wall = TextNote.Create(doc, view_rb.Id);
+                }
+
+
+
+            }
 
             return Result.Succeeded;
         }
---
+++ ba_roombook/ba_roombook/RoomDict.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ba_roombook
+{
+    public class RoomDict<TKey, TValue> : Dictionary<TKey, TValue> where TValue : new()
+    {
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                TValue val;
+                if (!TryGetValue(key, out val))
+                {
+                    val = new TValue();
+                    Add(key, val);
+                }
+                return val;
+            }
+            set { base[key] = value; }
+        }
+    }
+}
